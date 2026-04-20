289e5b926d5cd5e0538633915ab8bcaeade1830c
Miguel de Macedo Nunes <miguel.macedo@dcx.ufpb.br>
2026-04-20 11:43:08 -0300

    Jogo do Mario

---
+++ script.js
@@ -0,0 +1,52 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const alerGameOver = document.getSelection(".alert-game-over");
+
+const jump = () => {
+    mario.classList.add("jump");
+
+    setTimeout(() => {
+        mario.classList.remove("jump");
+    }, 500)
+}
+
+    //loop para o jogo só acabar se corresponder a condição
+const loop = setInterval(() => {
+
+    //criar variável de posição
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = parseFloat(window.getComputedStyle(mario).bottom);
+
+
+    const pipeWidth = pipe.offsetWidth;
+    const marioHeight = mario.offsetHeight;
+
+    
+    //Aqui o jogo acaba
+    if(pipePosition <= pipeWidth && pipePosition > 0 && marioPosition < marioHeight){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+         mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "imagens/game-over.png ";
+        mario.style.width = "75px";
+        mario.style.marginLeft = "50px";
+        alerGameOver.style.display = block;
+        alertGameOver.style.opacity = 1;
+        
+        
+        
+
+        clearInterval(loop);
+
+    }
+}, 10)
+
+
+document.addEventListener("keydown", jump);
+document.addEventListener("touchstart", jump);
+document.addEventListener("click", jump);
+
